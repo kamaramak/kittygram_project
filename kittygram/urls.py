086c30870c57fc76b6bc5b72afacb92772315ad5
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-07-21 15:17:52 +0300

    Закончена работа с этим проектом

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import include, path
 
-from cats.views import CatList, CatDetail
+# from cats.views import CatList, CatDetail
+
+from rest_framework.routers import SimpleRouter
+
+from cats.views import CatViewSet
+
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view()),
+   # path('cats/', CatList.as_view()),
+   # path('cats/<int:pk>/', CatDetail.as_view()),
+   path('', include(router.urls))
 ]
